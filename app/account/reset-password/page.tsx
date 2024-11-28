b6477e94ffe8159062312b767f8014fc13ba064f
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-11-28 17:12:45 +0900

    fix: missing-suspense-with-csr-bailout

--- app/account/reset-password/page.tsx
+++ app/account/reset-password/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react'
 import { ResetPasswordForm } from './components/ResetPasswordForm'
 
 function ResetPassword() {
-  return <ResetPasswordForm />
+  return (
+    <Suspense>
+      <ResetPasswordForm />
+    </Suspense>
+  )
 }
 
 export default ResetPassword
