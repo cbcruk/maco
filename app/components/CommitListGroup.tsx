d6eecd30238fee7caf768a049f44b231387fb765
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-01-04 09:27:42 +0900

    fix: 그룹 레이블 padding값 수정

--- app/components/CommitListGroup.tsx
+++ app/components/CommitListGroup.tsx
@@ -7,7 +7,7 @@ type CommitListGroupProps = PropsWithChildren<{
 export function CommitListGroup({ label, children }: CommitListGroupProps) {
   return (
     <div className="py-2">
-      <div className="sticky top-[56px] bg-white dark:bg-gray-950 px-4 py-4 text-sm text-gray-400">
+      <div className="sticky top-[56px] bg-white dark:bg-gray-950 px-4 pb-2 text-sm text-gray-400">
         {label}
       </div>
       <div data-part="list">{children}</div>
