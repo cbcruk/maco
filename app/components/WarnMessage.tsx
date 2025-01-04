8d413646b8c047151909aae190c19083d091a1d2
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-01-04 09:22:29 +0900

    fix: `inline-block` 줄바꿈을 위한 속성 수정

--- app/components/WarnMessage.tsx
+++ app/components/WarnMessage.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export function WarnMessage({ children }: PropsWithChildren) {
   return (
-    <p className="inline-flex p-2 rounded-lg bg-yellow-400 text-gray-800 font-medium empty:none">
+    <p className="inline-block p-2 rounded-lg bg-yellow-400 text-gray-800 font-medium empty:none">
       {children}
     </p>
   )
