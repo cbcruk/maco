8a517d4bf25d15362bbf0a3585ac7cdd3cff3822
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-04-20 12:54:44 +0900

    fix: 개인정보 업데이트 완료 후 캐시제거

--- app/account/_components/FormUser.actions.ts
+++ app/account/_components/FormUser.actions.ts
@@ -2,13 +2,24 @@
 
 import { UserService } from '@/services/User'
 import { Effect } from 'effect'
+import { revalidatePath } from 'next/cache'
 
 export async function updateUser(_: unknown, formData: FormData) {
   return Effect.gen(function* () {
     const userService = yield* UserService
     const id = formData.get('id') as string
-    const result = yield* userService.updateUser(id, {
+
+    yield* userService.updateUser(id, {
       name: formData.get('name') as string,
     })
-  }).pipe(Effect.provide(UserService.Default), Effect.runPromise)
+  }).pipe(
+    Effect.provide(UserService.Default),
+    Effect.match({
+      onSuccess: () => {
+        revalidatePath('/account')
+      },
+      onFailure: () => Effect.void,
+    }),
+    Effect.runPromise
+  )
 }
